9b131819efb06338c99858215d7f7c9009325b2c
cresian <ianracho21@gmail.com>
2024-04-07 18:03:27 +0800

    commit 5

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,4 +1,4 @@
-const config  = require('config.json');
+const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
@@ -13,7 +13,7 @@ async function initialize() {
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql'});
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
 
     // init models and add them to the exported db object
     db.Account = require('../accounts/account.model')(sequelize);
@@ -22,7 +22,7 @@ async function initialize() {
     // define relationships
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
-
+    
     // sync all models with database
     await sequelize.sync();
 }
--- accounts/refresh-token.model.js
+++ accounts/refresh-token.model.js
@@ -5,17 +5,18 @@ module.exports = model;
 function model(sequelize) {
     const attributes = {
         token: { type: DataTypes.STRING },
-        expires: { type: DataTypes.DATA },
-        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
-        createdByIp: { type: DataTypes.STRING }, 
+        expires: { type: DataTypes.DATE },
+        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        createdByIp: { type: DataTypes.STRING },
         revoked: { type: DataTypes.DATE },
-        revokedByIp: { type: DataTypes.STRING },  
+        revokedByIp: { type: DataTypes.STRING },
         replacedByToken: { type: DataTypes.STRING },
         isExpired: {
             type: DataTypes.VIRTUAL,
-            get() {return Date.now() >= this.expires; }
+            get() { return Date.now() >= this.expires; }
         },
         isActive: {
+            type: DataTypes.VIRTUAL,
             get() { return !this.revoked && !this.isExpired; }
         }
     };
